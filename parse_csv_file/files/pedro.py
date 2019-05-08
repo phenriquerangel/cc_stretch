9e99a1b65b97350b870b28e3b9803149841b113e
root <root@BRRIOWN012297.stone.local>
2019-05-08 13:46:38 +0000

    challenge 1 finished

--- parse_csv_file/files/pedro.py
+++ parse_csv_file/files/pedro.py
@@ -3,9 +3,11 @@ with open('example.csv','r') as text_file:
     for line in text_file.readlines():
         value_list = str(line).split(",")
         neighbor= value_list[3]
+        hostname= value_list[0]
         interface= value_list[2]
         ip=value_list[1]
-        #print(neighbor)
+        #print(hostname)
         if first_line is False:
-            print("-{} interface=\"{}\"\n ip=\"{}\"\n".format(neighbor,interface,ip))
+            with open(hostname, 'a') as txt_file:
+                txt_file.write("-{} interface=\"{}\"\n ip=\"{}\"\n".format(neighbor,interface,ip))
         first_line = False
